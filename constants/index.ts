3637b654b54112b6055945ea87979068f01305f6
Pablo Domingo <pablodomingo1993@gmail.com>
2024-05-02 00:37:04 -0300

    Se ha creado la sidebar

---
+++ constants/index.ts
@@ -0,0 +1,30 @@
+export const sidebarLinks = [
+    {
+        label:'Home',
+        route:'Home',
+        imgUrl:'/icons/Home.svg',
+    },
+    {
+        label:'Upcoming',
+        route:'upcoming',
+        imgUrl:'/icons/upcoming.svg',
+    },
+    {
+        label:'Previous',
+        route:'previous',
+        imgUrl:'/icons/previous.svg',
+    },
+    {
+        label:'Recordings',
+        route:'recordings',
+        imgUrl:'/icons/Video.svg',
+    },
+    {
+        label:'Personal Room',
+        route:'personal-room',
+        imgUrl:'/icons/add-personal.svg',
+    },
+
+
+
+]
